7cb907fb27b87d5bb2f4b3324950b95ec4495b58
Documenter.jl <documenter@juliadocs.github.io>
2021-10-21 02:17:00 +0000

    build based on c65790f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageSSD","category":"page"},{"location":"#DemoPackageSSD","page":"Home","title":"DemoPackageSSD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageSSD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageSSD]","category":"page"}]
+}
